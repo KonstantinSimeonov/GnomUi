754dcce2b930a775cf59d23f61ca70dadf473968
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-09-19 01:29:25 +0300

    Added id in element

--- GnomUi/Contracts/IElement.cs
+++ GnomUi/Contracts/IElement.cs
@@ -2,6 +2,7 @@
 {
     public interface IElement
     {
+        string Id { get; set; }
         IStyle Style { get; set; }
         void Display(int x, int y);
     }
--- GnomUi/Element.cs
+++ GnomUi/Element.cs
@@ -6,6 +6,7 @@
 
     public abstract class Element : IElement
     {
+        public string Id { get; set; }
 
         public IStyle Style { get; set; }
 
